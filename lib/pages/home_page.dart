ad3c3139e93a7915c450c04d2b8088278b6138a6
waynelijia <waynelijia@didichuxing.com>
2019-02-19 19:27:17 +0800

    添加Dio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('商城首页'),
       ),
     );
+  }
+
+  void getHttp() async{
+    try {
+      Response response;
+      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=美女');
+      print(response);
+    } catch(e) {
+      return print(e);
+    }
   }
 }
