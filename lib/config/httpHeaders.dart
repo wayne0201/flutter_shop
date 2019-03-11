5c129f60940705bd07b86843f1c5c26bd13808d0
waynelijia <waynelijia@didichuxing.com>
2019-03-11 23:45:02 +0800

    首页修改

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,12 @@
+const httpHeaders = {
+  "Accept": "application/json, text/plain, */*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "zh-CN,zh;q=0.9",
+  "Connection": "keep-alive",
+  "Content-Type": "application/json",
+  "Cookie": "_ga=GA1.2.946113108.1551855489; _gid=GA1.2.846761425.1551855489; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1551855489; _gat=1; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1551855590|1551855493; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1551855587",
+  "Host": "time.geekbang.org",
+  "Origin": "https://time.geekbang.org",
+  "Referer": "https://time.geekbang.org/",
+  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:convert';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -10,70 +14,256 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎你来到美好人间';
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('美好人家'),
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类型',
-                  helperText: '请输入你喜欢的类型',
-                ),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-              )
-            ],
+          appBar: AppBar(
+            title: Text('百姓生活+'),
           ),
+          body: FutureBuilder(
+            future: getHomePageContent(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var data = json.decode(snapshot.data.toString());
+                Map _date = data['data'];
+                List<Map> swiper = (_date['slides'] as List).cast();
+                List<Map> navgatorList = (_date['category'] as List).cast();
+                List<Map> recommendList = (_date['recommend'] as List).cast();
+                String adPicture = _date['advertesPicture']['PICTURE_ADDRESS'];
+                String leaderPhone = _date['shopInfo']['leaderPhone'];
+                String leaderImage = _date['shopInfo']['leaderImage'];
+                return SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      SwiperDiy(
+                        swiperDateList: swiper,
+                      ),
+                      TopNavigator(
+                        navigatorList: navgatorList,
+                      ),
+                      AdBanner(
+                        adPicture: adPicture,
+                      ),
+                      LeaderPhone(
+                        leaderImage: leaderImage,
+                        leaderPhone: leaderPhone,
+                      ),
+                      Recommend(
+                        recommendList: recommendList,
+                      )
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                  child: Text(
+                    '加载中...',
+                  ),
+                );
+              }
+            },
+          )),
+    );
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+
+  SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemBuilder: (BuildContext content, int index) {
+          return Image.network(
+            "${swiperDateList[index]['image']}",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print("点击了导航");
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item["image"],
+            width: ScreenUtil().setWidth(95),
+          ),
+          Text(
+            item["mallCategoryName"],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (navigatorList.length > 10) {
+      navigatorList.removeRange(10, navigatorList.length);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5.0),
+        children: navigatorList.map((item) {
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class AdBanner extends StatelessWidget {
+  final String adPicture;
+
+  AdBanner({Key key, this.adPicture}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(
+        adPicture,
+      ),
+    );
+  }
+}
+
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage;
+  final String leaderPhone;
+
+  LeaderPhone({Key key, this.leaderImage, this.leaderPhone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL() async {
+    String url = 'tel:$leaderPhone';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'url不能进行访问！';
+    }
+  }
+}
+
+class Recommend extends StatelessWidget {
+  final List recommendList;
+
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  Widget _titleWidget() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 5.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: Colors.black12,
+          ),
+        ),
+      ),
+      child: Text(
+        '商品推荐',
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  Widget _item(index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(350),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left: BorderSide(
+              width: 1,
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              recommendList[index]['image'],
+            ),
+            Text(
+              "￥${recommendList[index]['mallPrice']}",
+            ),
+            Text(
+              "￥${recommendList[index]['mallPrice']}",
+              style: TextStyle(
+                decoration: TextDecoration.lineThrough,
+                color: Colors.grey,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  void _choiceAction() {
-    print('开始选择你喜欢的类型');
-    if (typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              title: Text('美女类型不能为空'),
-            ),
-      );
-    } else {
-      getHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          showText = val['data']['name'];
-        });
-      });
-    }
+  Widget _recommedList() {
+    return Container(
+      height: ScreenUtil().setHeight(350),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context, index) {
+          return _item(index);
+        },
+      ),
+    );
   }
 
-  Future getHttp(String typeText) async {
-    try {
-      Response response;
-      var data = {'name': typeText};
-      response = await Dio().get(
-        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
-        queryParameters: data,
-      );
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(410),
+      margin: EdgeInsets.only(top: 10.0),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommedList(),
+        ],
+      ),
+    );
   }
 }
