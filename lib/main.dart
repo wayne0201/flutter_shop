3a233b2a1a6a36f65ad51ecdb649628e91092abb
wayne0201 <614418910@qq.com>
2019-03-30 23:24:48 +0800

    添加provide

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import './provide/counter.dart';
 import './pages/index_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  var counter = Counter();
+  var providers = Providers();
+
+  providers..provide(Provider<Counter>.value(counter));
+
+  runApp(ProviderNode(
+    providers: providers,
+    child: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Container(
