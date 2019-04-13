a637bb3b703a5dc0db15a95089d3a260bf73b345
wayne0201 <614418910@qq.com>
2019-04-13 14:32:40 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import 'package:fluro/fluro.dart';
 
+import './routers/routes.dart';
+import './routers/application.dart';
+
 import './provide/counter.dart';
 import './provide/child_category.dart';
 import './provide/category_goods_list.dart';
@@ -12,10 +15,7 @@ void main() {
   final counter = Counter();
   final childCategory = ChildCategory();
   final categoryGoodsProvide = CategoryGoodsProvide();
-
   final providers = Providers();
-  final router = Router();
-
   providers
     ..provide(Provider<Counter>.value(counter))
     ..provide(Provider<ChildCategory>.value(childCategory))
@@ -27,9 +27,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
+
     return Container(
       child: MaterialApp(
         title: '百姓生活',
+        onGenerateRoute: Application.router.generator,
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.pink,
